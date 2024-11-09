9e8dbcecd815695068b8510f3bd3ca6885001c4b
Raghu128 <119107622+Raghu128@users.noreply.github.com>
2024-11-09 23:10:57 +0530

    add files

---
+++ index.js
@@ -0,0 +1,44 @@
+// const express = require('express')
+// const userRouter = require('./controllers/user');
+// const adminRouter = require('./controllers/admin');
+
+import express from 'express';
+// import userRouter from './routes/user.js';
+// import adminRouter from './routes/admin.js';
+import cors from 'cors'
+
+const app = express();
+const port = process.env.PORT || 3000;  
+
+const jokes = [{id:1, joke:"Joke1"}, {id:2, joke:"Joke2"}, {id:3, joke:"Joke3"}, {id:4, joke:"Joke4"}, {id:5, joke:"Joke5"}]
+
+// app.use(express.urlencoded({extended:false}));
+app.use(
+    cors({
+      origin: ["https://deploy-mern-lwhq.vercel.app"],
+      credentials: true,
+    })
+  );
+
+// app.use((req, res, next) => {
+    
+//     req.username = "RaghuKumar"
+//     next()
+// })
+
+// app.use((req, res, next) => {
+    
+//     console.log(req.username);
+//     next()
+// })
+
+// app.use("/user", userRouter);
+// app.use('/admin', adminRouter);
+app.get("/", (req, res) => {
+    res.json(jokes);
+})
+
+
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+})
---
+++ routes/admin.js
@@ -0,0 +1,22 @@
+// const express = require('express')
+import express from 'express'
+
+
+const router = express.Router();
+
+const jokes = [{id:1, joke:"Joke1"}, {id:2, joke:"Joke2"}, {id:3, joke:"Joke3"}, {id:4, joke:"Joke4"}, {id:5, joke:"Joke5"}]
+
+router.get('/', (req, res) => {
+    res.send("<h1>Hello itsss admin Get method calling</h1>");
+});
+
+// app.get('/api/jokes', (req, res) => {
+//     res.send(jokes);
+// })
+
+router.get('/about', (req, res) => {
+    res.send("<h1> admin About Page </h1>")
+})
+
+// module.exports = router;
+export default router;
